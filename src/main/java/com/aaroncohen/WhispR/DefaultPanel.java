10cfb4704661b47549f6af00fb92faa79d98c417
Aaron <ajccool@icloud.com>
2022-02-11 16:10:49 -0700

    Bug fixes

--- src/main/java/com/aaroncohen/WhispR/DefaultPanel.java
+++ src/main/java/com/aaroncohen/WhispR/DefaultPanel.java
@@ -26,11 +26,25 @@ public class DefaultPanel {
 
         //when the join room button is clicked
         joinButton.addActionListener((e -> {
-            //if statements to change colors of the labels if they're empty
+            //if the name field is empty make the label red
             if (nameField.getText().equals("")) nameLabel.setForeground(ERROR_RED); else nameLabel.setForeground(defaultTextColor);
-            if (roomField.getText().equals("")) roomLabel.setForeground(ERROR_RED); else {
-                //if the room field isn't empty try to join the room
-                if (!nameField.getText().equals("") && Integer.parseInt(roomField.getText()) > 4096 && Integer.parseInt(roomField.getText()) < 35565) {
+
+            //test if the room is valid
+            boolean validRoom = true;
+            try {
+                if (Integer.parseInt(roomField.getText()) > 4096 && Integer.parseInt(roomField.getText()) < 35565) {
+                    //do nothing, the room is valid
+                } else validRoom = false;
+            } catch (Exception ex) {validRoom = false;}
+
+            //check if the room is valid
+            if (!validRoom) {
+                roomLabel.setForeground(ERROR_RED);
+                roomField.setText("");
+            } else {
+                roomLabel.setForeground(defaultTextColor);
+                //if the name field isn't empty try to join the room
+                if (!nameField.getText().equals("")) {
                     //change screen
                     ChatPanel chatPanel = new ChatPanel(parent, nameField.getText(), Integer.parseInt(roomField.getText()));
                     parent.add(chatPanel.getPanel());
@@ -39,11 +53,6 @@ public class DefaultPanel {
                     //create chatNetworker
                     ChatNetworker chatNetworker = new ChatNetworker(parent, chatPanel, Integer.parseInt(roomField.getText()));
                     chatNetworker.start();
-
-                //else statement to set color of room label to red if it isn't valid
-                } else {
-                    roomLabel.setForeground(ERROR_RED);
-                    roomField.setText("");
                 }
             }
         }));
